4c785cba88076596571412d7d72e39e231dc0f66
ghanekarpayal11213 <126890237+ghanekarpayal11213@users.noreply.github.com>
2023-03-03 20:22:09 +0530

    day 4 assignment

---
+++ day4.py
@@ -0,0 +1,6 @@
+def find_smallest_number(list):
+    smallest = min(list1)
+    return smallest
+
+list1 = [5,7,9,14,10,20,4]
+print("smallest number in the list is:",find_smallest_number(list1))    
